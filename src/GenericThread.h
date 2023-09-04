b3722fb6fa654699bd792104bb4e3a06e11d7a28
mtmal <mt@mal>
2023-09-04 22:08:15 +0100

    Added a generic thread class and a relevant test for it

---
+++ src/GenericThread.h
@@ -0,0 +1,111 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2023 Mateusz Malinowski
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <atomic>
+#include <pthread.h>
+#include <semaphore.h>
+
+template <typename Derived>
+class GenericThread
+{
+public:
+    /**
+     * Basic class constructor.
+     */
+    GenericThread() : mThread(0)
+    {
+        pthread_mutex_init(&mMutex, nullptr);
+        sem_init(&mSemaphore, 0, 0);
+        mRun.store(false, std::memory_order_relaxed);
+    }
+
+    /**
+     * Class destructor that stops the thread. It does not call the thread cancellation nor process any thread-generated outputs.
+     */
+    virtual ~GenericThread()
+    {
+        stopThread(false);
+        sem_destroy(&mSemaphore);
+        pthread_mutex_destroy(&mMutex);
+    }
+
+    /**
+     * Starts the thread.
+     */
+    bool startThread()
+    {
+        bool retVal = (0 == pthread_create(&mThread, nullptr, GenericThread::startThread, static_cast<Derived*>(this)));
+        mRun.store(retVal, std::memory_order_release);
+        return retVal;
+    }
+
+    /**
+     * Stops the thread and returns any data it may have created.
+     *  @param force a flag to indicate if the termination should be forced by cancelling the thread.
+     *  @param[out] threadReturn data returned by the thread. nullptr assumes no return data.
+     */
+    void stopThread(const bool force = false, void* threadReturn = nullptr)
+    {
+        mRun.store(false, std::memory_order_relaxed);
+        if (mThread > 0)
+        {
+            if (force)
+            {
+                pthread_cancel(mThread);
+            }
+            pthread_join(mThread, &threadReturn);
+            mThread = 0;
+        }
+    }
+
+    /**
+     *  @return true if the thread is running.
+     */
+    inline bool isRunning() const
+    {
+        return mRun.load(std::memory_order_acquire);
+    }
+
+protected:
+    /** Mutex for locking resources. */
+    pthread_mutex_t mMutex;
+    /** Semaphore for thread synchronisation. */
+    sem_t mSemaphore;
+
+private:
+    /**
+     * Starts the thread. @p instance needs to implement theadBody function.
+     *  @param instance an instance of the derived class.
+     *  @return thread return values.
+     */
+    static void* startThread(void* instance)
+    {
+        return static_cast<Derived*>(instance)->theadBody();
+    }
+
+    /** Pthread related to this class. */
+    pthread_t mThread;
+    /** A flag indicating if the thread is running. */
+    std::atomic<bool> mRun;
+};
---
+++ tests/test_Thread.cpp
@@ -0,0 +1,64 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2023 Mateusz Malinowski
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+////////////////////////////////////////////////////////////////////////////////
+
+#include <cstdio>
+#include <string>
+#include <unistd.h>
+#include <GenericThread.h>
+
+std::atomic<int> globalCounter = 0;
+
+class Counter : public GenericThread<Counter>
+{
+public:
+    Counter(const int maxIter, const std::string& name) : GenericThread<Counter>(), mMaxIter(maxIter), mCounter(0), mName(name) {}
+
+    virtual ~Counter()
+    {
+        printf("My name: %s, local counter: %d, global counter: %d \n", mName.c_str(), mCounter, globalCounter.load(std::memory_order_relaxed));
+    }
+
+    void* theadBody()
+    {
+        while (mCounter < mMaxIter)
+        {
+            ++mCounter;
+            globalCounter.fetch_add(1);
+        }
+        return nullptr;
+    }
+
+private:
+    int mMaxIter;
+    int mCounter;
+    std::string mName;
+};
+
+int main()
+{
+    Counter c1 = Counter(10, "C1");
+    Counter c2 = Counter(40, "C2");
+    c1.startThread();
+    c2.startThread();
+    sleep(1);
+    return 0;
+}
